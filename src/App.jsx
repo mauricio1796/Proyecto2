882d00b08a94a8a9de925bb162b9a672a74a6a5b
LaMaquina <ricardo.mosquerab@uniagustiniana.edu.co>
2025-03-19 13:26:02 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import Otro from './Otro'
+import Estado from './Estado'
+import String from './String'
+import Booleano from './Booleano'
+import Objeto from './Objeto'
+import Clase from './Clase'
+
+function App() {
+  return (
+    <>
+    
+    {/*  <Otro />
+      <Estado />
+      <String />
+      <Booleano />
+      <Objeto />*/}
+      <Clase />
+    </>
+  )
+}
+
+export default App
---
+++ src/Otro.jsx
@@ -0,0 +1,9 @@
+function Otro() {
+  return (
+    <>
+      <h2>Otro</h2>
+    </>
+  );
+}
+
+export default Otro;
